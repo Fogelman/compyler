6d0d550e1c24409ce943ffbd202726cb269c8a37
Fogelman <ddfogelman@gmail.com>
2020-02-23 19:56:55 -0300

    UPDATE - Parsing tokens

--- parser.py
+++ parser.py
@@ -1,10 +1,25 @@
+from tokenizer import Tokenizer
+
+
 class Parser:
     tokens = None
-    
-    @staticmethod
-    parseExpression():
-        pass
 
     @staticmethod
-    run(code):
-        pass
+    def parseExpression():
+        Parser.tokens.selectNext()
+        # while Parser.tokens.actual.type != "EOF":
+        #     print(Parser.tokens.actual.value)
+        #     Parser.tokens.selectNext()
+
+        currentToken = Parser.tokens.actual
+        result = 0
+        if(currentToken.type == "INT"):
+            pass
+        else:
+            raise Exception(
+                "[-] ERROR Could not decode input. Check semantics.")
+
+    @staticmethod
+    def run(code):
+        Parser.tokens = Tokenizer(code)
+        Parser.parseExpression()
--- tokenizer.py
+++ tokenizer.py
@@ -1,10 +1,36 @@
-from token import Token
+from utils import Token
+
 
 class Tokenizer:
-    def __init__(self, origin, position, actual):
+    def __init__(self, origin, position=0, actual=None):
         self.origin = origin
         self.position = position
         self.actual = actual
 
-    def selectNext():
-        pass
+    def selectNext(self):
+        lenOrigin = len(self.origin)
+        for i in self.origin[self.position:]:
+            if i.strip() == "":
+                self.position += 1
+            else:
+                break
+
+        if self.position >= lenOrigin:
+            self.actual = Token("", "EOF")
+        elif self.origin[self.position] == "+":
+            self.actual = Token("+", "PLUS")
+        elif self.origin[self.position] == "-":
+            self.actual = Token("-", "MINUS")
+        elif self.origin[self.position].isdigit():
+            digit = ""
+            i = self.position
+            while i < lenOrigin and self.origin[i].isdigit():
+                digit += self.origin[self.position]
+                i += 1
+            self.actual = Token(digit, "INT")
+
+        else:
+            raise Exception(
+                f"Could not decode character at position {self.position}")
+
+        self.position += len(self.actual.value)
