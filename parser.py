de28b4695d55304fd0a8017b3767ee23e50744e1
Fogelman <ddfogelman@gmail.com>
2020-02-28 12:58:13 -0300

    UPDATE add + and / operations

--- parser.py
+++ parser.py
@@ -5,7 +5,7 @@ class Parser:
     tokens = None
 
     @staticmethod
-    def parseExpression():
+    def parseTerm():
         tokens = Parser.tokens
         tokens.selectNext()
         result = 0
@@ -14,30 +14,43 @@ class Parser:
             result = int(tokens.actual.value)
             tokens.selectNext()
 
-            while not tokens.actual.type == "EOF":
-                if tokens.actual.type == "PLUS":
+            while tokens.actual.type in ["DIVIDE", "MULTIPLY"]:
+                if tokens.actual.type == "MULTIPLY":
                     tokens.selectNext()
                     if tokens.actual.type == "INT":
-                        result += int(tokens.actual.value)
+                        result *= int(tokens.actual.value)
                     else:
-                        raise Exception("[-] Could not decode input.")
-                elif tokens.actual.type == "MINUS":
+                        raise Exception("[-] unexpected token.")
+                elif tokens.actual.type == "DIVIDE":
                     tokens.selectNext()
                     if tokens.actual.type == "INT":
-                        result -= int(tokens.actual.value)
+                        result /= int(tokens.actual.value)
                     else:
-                        raise Exception("[-] Could not decode input.")
-                else:
-                    raise Exception("[-] Could not decode input.")
+                        raise Exception("[-] unexpected token.")
                 tokens.selectNext()
-
             return result
         else:
             raise Exception(
-                "[-] ERROR Could not decode input.")
+                "[-] unexpected token.")
+
+    @staticmethod
+    def parseExpression():
+        tokens = Parser.tokens
+        result = Parser.parseTerm()
+
+        while tokens.actual.type in ["PLUS", "MINUS"]:
+            if tokens.actual.type == "PLUS":
+                result += Parser.parseTerm()
+            elif tokens.actual.type == "MINUS":
+                result -= Parser.parseTerm()
+            else:
+                raise Exception("[-] unexpected token.")
+        return result
 
     @staticmethod
     def run(code):
         Parser.tokens = Tokenizer(code)
         parsed = Parser.parseExpression()
+        if Parser.tokens.actual.type != "EOF":
+            raise Exception("[-] unexpected token")
         return parsed
