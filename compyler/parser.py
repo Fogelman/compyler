b321a62de10acdb7ae0c1cdd0e2ccfa99443b277
Fogelman <ddfogelman@gmail.com>
2020-04-03 18:08:37 -0300

    UPDATE fixing tokens

--- compyler/parser.py
+++ compyler/parser.py
@@ -11,11 +11,9 @@ class Parser:
         tokens.selectNext()
         result = []
         if tokens.actual.type == "LBRACE":
-
             tokens.selectNext()
             while tokens.actual.type != "RBRACE":
                 result.append(Parser.parseCommand())
-                tokens.selectNext()
         else:
             raise Exception("[-] unexpected token.")
 
@@ -25,42 +23,47 @@ class Parser:
     @staticmethod
     def parseCommand():
         tokens = Parser.tokens
-        # tokens.selectNext()
 
         if tokens.actual.type == "IDENTIFIER":
             result = Assignment(tokens.actual.value)
             tokens.selectNext()
             if tokens.actual.type != "EQUAL":
                 raise Exception("[-] unexpected token.")
+            tokens.selectNext()
             result.children = [Parser.parseExpression()]
 
         elif tokens.actual.type == "COMMAND":
-            result = Echo(tokens.actual.value, [Parser.parseExpression()])
-        else:
-            result = Parser.parseBlock()
+            value = tokens.actual.value
+            tokens.selectNext()
+            result = Echo(value, [Parser.parseExpression()])
 
-        tokens.selectNext()
+        else:
+            return Parser.parseBlock()
+
         if tokens.actual.type != "SEMI":
             raise Exception("[-] unexpected token.")
 
+        tokens.selectNext()
         return result
 
     @staticmethod
     def parseFactor():
         tokens = Parser.tokens
-        tokens.selectNext()
         result = None
         if tokens.actual.type == "INT":
             result = IntVal(int(tokens.actual.value))
-        elif tokens.actual.type == "OPEN":
-            result = Parser.parseExpression()
             tokens.selectNext()
+        elif tokens.actual.type == "OPEN":
+            tokens.selectNext()
+            result = Parser.parseExpression()
             if(tokens.actual.type != "CLOSE"):
                 raise Exception("[-] unexpected token.")
             tokens.selectNext()
         elif tokens.actual.type == "PLUS":
+            tokens.selectNext()
             result = UnOp("PLUS", [Parser.parseFactor()])
         elif tokens.actual.type == "MINUS":
+            tokens.selectNext()
             result = UnOp("MINUS", [Parser.parseFactor()])
         elif tokens.actual.type == "IDENTIFIER":
             result = Identifier(tokens.actual.value)
@@ -77,8 +80,10 @@ class Parser:
 
         while tokens.actual.type in ["DIVIDE", "MULTIPLY"]:
             if tokens.actual.type == "MULTIPLY":
+                tokens.selectNext()
                 result = BinOp("MULTIPLY", [result, Parser.parseFactor()])
             elif tokens.actual.type == "DIVIDE":
+                tokens.selectNext()
                 result = BinOp("DIVIDE", [result, Parser.parseFactor()])
         return result
 
@@ -88,8 +93,10 @@ class Parser:
         result = Parser.parseTerm()
         while tokens.actual.type in ["PLUS", "MINUS"]:
             if tokens.actual.type == "PLUS":
+                tokens.selectNext()
                 result = BinOp("PLUS", [result, Parser.parseTerm()])
             elif tokens.actual.type == "MINUS":
+                tokens.selectNext()
                 result = BinOp("MINUS", [result, Parser.parseTerm()])
         return result
 
